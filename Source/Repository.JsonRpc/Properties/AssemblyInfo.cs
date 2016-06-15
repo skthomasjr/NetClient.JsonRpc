d1e4b7e05cf5e38965542b59e63112c2ae9f5c31
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-15 05:45:28 -0600

    Initial source commit.

---
+++ Source/Repository.JsonRpc/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Repository.JsonRpc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Repository.JsonRpc")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b80bbf53-b7e3-43ad-a2b7-d244282ee475")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
